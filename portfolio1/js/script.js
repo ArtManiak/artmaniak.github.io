4f0d01486f7793ec9a4472b4511eb03480f3c202
Artur Haber <63986324+ArtManiak@users.noreply.github.com>
2020-12-11 15:23:00 +0100

    new project
    my portfolio

--- portfolio1/js/script.js
+++ portfolio1/js/script.js
@@ -1,5 +1,5 @@
-const toggleButton=document.getElementsByClassName('toggle-button')[0]
-const navLinki=document.getElementsByClassName('linki')[0]
-toggleButton.addEventListener('click',()=>{
+const toggleButton = document.getElementsByClassName('toggle-botton')[0]
+const navLinki = document.getElementsByClassName('linki')[0]
+toggleButton.addEventListener('click', () => {
     navLinki.classList.toggle('active')
 })
